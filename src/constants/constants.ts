ec1cc2b07c1a1b33f17469a2b74068ac6e67bdc9
Jordanoo <jordanhernandez86@gmail.com>
2022-02-13 00:02:27 +0100

    add service

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,2 @@
 export const API_URL = "https://api.coingecko.com/api/v3/";
+export const API_COINS_MARKET = `${API_URL}coins/markets?`;
---
+++ src/service/getCryptoCurrency.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { API_COINS_MARKET } from "../constants";
+
+export default async function getCryptoCurrency() {
+  const url = `${API_COINS_MARKET}vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false`;
+  try {
+    const response = await axios(url);
+    if (response.status === 200 && response.data) {
+      console.log(response.data, "response");
+      return response.data;
+    }
+  } catch (error) {
+    console.log(error, "error");
+  }
+}
