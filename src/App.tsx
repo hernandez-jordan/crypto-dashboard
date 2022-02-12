3aa7a2c5fe84ddb183660e18e52adc6c511a9312
Jordanoo <jordanhernandez86@gmail.com>
2022-02-12 21:30:59 +0100

    fix index

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline } from "@mui/material";
+import "./App.css";
+import ProfilePage from "./view/Profile/ProfilePage";
+import "./service/axios-config";
+
+// axios.defaults.baseURL = API_URL;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <ProfilePage />
     </div>
   );
 }
