4a95bb569232387f498c32f29f25a63adcb2682b
Jordanoo <jordanhernandez86@gmail.com>
2022-02-16 16:32:30 +0100

    add watchlist to profile

--- src/view/Profile/ProfilePage.tsx
+++ src/view/Profile/ProfilePage.tsx
@@ -3,6 +3,7 @@ import AccountBalance from "./AccountBalance";
 import AccountSecurity from "./AccountSecurity";
 import Header from "./Header";
 import Wallets from "./Wallets";
+import WatchList from "./WatchList";
 
 export default function ProfilePage() {
   return (
@@ -23,6 +24,9 @@ export default function ProfilePage() {
         </Grid>
         <Grid item lg={4} xs={12}>
           <AccountSecurity />
+        </Grid>
+        <Grid item xs={12}>
+          <WatchList />
         </Grid>
       </Grid>
     </Container>
