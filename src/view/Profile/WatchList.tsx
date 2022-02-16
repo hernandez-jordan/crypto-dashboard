edb5a06e50fb23d4bda96e69520a7095a809cad2
Jordanoo <jordanhernandez86@gmail.com>
2022-02-16 17:55:33 +0100

    add css styling

--- src/view/Profile/WatchList.tsx
+++ src/view/Profile/WatchList.tsx
@@ -1,8 +1,8 @@
-import { Label } from "@mui/icons-material";
 import { Avatar, Card, Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useCallback, useEffect, useState } from "react";
 import getCryptoCurrency from "../../service/getCryptoCurrency";
+import { isPositiveInteger } from "../../util/isPositiveInteger";
 
 export default function WatchList() {
   const [data, setData] = useState<[] | undefined>([]);
@@ -43,19 +43,47 @@ export default function WatchList() {
                   <Box p={3}>
                     <Box display="flex" alignItems="center">
                       <Avatar sx={{ my: 2 }} src={image} alt={name} />
-                      <Box display="flex" alignItems="center" sx={{display:"flex", flexDirection: "column", alignItems: "flex-start "}}>
+                      <Box
+                        display="flex"
+                        alignItems="center"
+                        sx={{
+                          display: "flex",
+                          flexDirection: "column",
+                          alignItems: "flex-start ",
+                          pl: "14px",
+                        }}
+                      >
                         <Typography>{name}</Typography>
                         <Typography variant="body2">{symbol}</Typography>
                       </Box>
                     </Box>
-                    <Box>
-                      <Typography>${current_price}</Typography>
-                      <Typography variant="body2">
+                    <Box paddingBottom={2} display="flex">
+                      <Typography variant="h5" noWrap sx={{ pr: 2 }}>
+                        ${current_price}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        sx={{ display: "flex", alignItems: "center" }}
+                        color={
+                          isPositiveInteger(Number(price_change_percentage_24h))
+                            ? "limegreen"
+                            : "red"
+                        }
+                      >
                         {price_change_percentage_24h}
                       </Typography>
                     </Box>
-                    <Box>
-                      <Label color="success">+$500</Label>
+                    <Box display="flex">
+                      <Box
+                        sx={{
+                          borderRadius: "10px",
+                          backgroundColor: "#8efb8e",
+                          color: "#0f990f",
+                          p: "2px 10px",
+                        }}
+                      >
+                        +$500
+                      </Box>
                       <Typography
                         variant="body2"
                         color="text.secondary"
